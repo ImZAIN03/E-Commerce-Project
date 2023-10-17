43d6fb11894422bc7b161cd84c09123fe94bf694
Muhammed Zain <muhammedzain2256@gmail.com>
2023-10-17 11:45:32 +0500

    Created Product Card

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,14 @@ import Layout from './../../components/layout/Layout';
 import myContext from '../../context/data/myContext';
 import HeroSection from '../../components/heroSection/HeroSection';
 import Filter from '../../components/filter/Filter';
+import ProductCard from '../../components/productCard/ProductCard';
 
 function Home() {  
   return (
     <Layout>
       <HeroSection/>
       <Filter/>
+      <ProductCard/>
     </Layout>
   )
 }
