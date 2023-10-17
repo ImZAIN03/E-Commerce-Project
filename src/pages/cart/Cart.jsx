d0e875767a5b9201e242c66785aa131b8730b983
Muhammed Zain <muhammedzain2256@gmail.com>
2023-10-17 21:01:04 +0500

    Added Dashboard

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -22,7 +22,7 @@ function Cart() {
                 <div className="mt-5 sm:mt-0">
                   <h2 className="text-lg font-bold text-gray-900" style={{ color: mode === 'dark' ? 'white' : '' }}>This is title</h2>
                   <h2 className="text-sm  text-gray-900" style={{ color: mode === 'dark' ? 'white' : '' }}>desc</h2>
-                  <p className="mt-1 text-xs font-semibold text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>₹100</p>
+                  <p className="mt-1 text-xs font-semibold text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>Rs 100</p>
                 </div>
                 <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -38,17 +38,17 @@ function Cart() {
           <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3" style={{ backgroundColor: mode === 'dark' ? 'rgb(32 33 34)' : '', color: mode === 'dark' ? 'white' : '', }}>
             <div className="mb-2 flex justify-between">
               <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>Subtotal</p>
-              <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>₹100</p>
+              <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>Rs 100</p>
             </div>
             <div className="flex justify-between">
               <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>Shipping</p>
-              <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>₹20</p>
+              <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>Rs 20</p>
             </div>
             <hr className="my-4" />
             <div className="flex justify-between mb-3">
               <p className="text-lg font-bold" style={{ color: mode === 'dark' ? 'white' : '' }}>Total</p>
               <div className>
-                <p className="mb-1 text-lg font-bold" style={{ color: mode === 'dark' ? 'white' : '' }}>₹200</p>
+                <p className="mb-1 text-lg font-bold" style={{ color: mode === 'dark' ? 'white' : '' }}>Rs 200</p>
               </div>
             </div>
             <Modal  />
