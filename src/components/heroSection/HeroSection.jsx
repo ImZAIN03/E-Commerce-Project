0cb021ef961e89d91009220b4e6c567bf8436889
Muhammed Zain <muhammedzain2256@gmail.com>
2023-10-17 10:35:40 +0500

    Hero Section Initial Changes

---
+++ src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+function HeroSection() {
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000, 
+  };
+
+  const images = [
+    'https://images.pexels.com/photos/1087727/pexels-photo-1087727.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1080721/pexels-photo-1080721.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  ];
+
+  return (
+    <div className="w-4/5 mx-auto rounded-lg mt-4"> 
+      <Slider {...sliderSettings}>
+        {images.map((image, index) => (
+          <div key={index}>
+            <img src={image} alt={`Slide ${index + 1}`} className="w-full h-auto object-fit" style={{ height: "400px" }} /> 
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
+
+export default HeroSection;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,13 @@
 import React, { useContext } from 'react'
 import Layout from './../../components/layout/Layout';
 import myContext from '../../context/data/myContext';
+import HeroSection from '../../components/heroSection/HeroSection';
 
-function Home() {
-  const context = useContext(myContext);
-  // console.log(context);
-  // const {name,rollno} = context;
-  const {state,color} = context;
+function Home() {  
   return (
     <Layout>
-      {/* <h1>Name: {state.name}</h1> */}
-      {/* <h1>Roll No: {state.rollno}</h1> */}
-      {/* <h1>My Color: {color}</h1> */}
+      <HeroSection/>
+    
     </Layout>
   )
 }
